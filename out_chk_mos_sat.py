c2078de8f1dcdba56bcab7923106d532c5dce548
houndofFOX <samuel.akira@gmail.com>
2018-11-16 20:47:06 -0800

    Updated logging to use object logger.  Modified logging format

--- out_chk_mos_sat.py
+++ out_chk_mos_sat.py
@@ -4,9 +4,9 @@ import sys
 import logging
 import os.path
 
-LOG_LVL = logging.INFO
+LOG_LVL = logging.DEBUG
 
-def main():
+def main(Logger):
     mos_sec = 0
     found_v = [0, 0]
 
@@ -15,7 +15,7 @@ def main():
     for line in open(sys.argv[1], 'r'):
         # Get to the MOSFET parameters section
         if "**** MOSFETS" in line:
-            logging.debug("Found MOSFETS")
+            Logger.debug("Found MOSFETS")
             mos_sec = 1
         # Process lines in the MOSFET section
         if mos_sec:
@@ -25,44 +25,47 @@ def main():
 
             # Calcualte VDS - VSAT
             if found_v[0] and found_v[1]:
-                logging.debug("{}, {}".format(len(VDS), len(VDSAT)))
+                Logger.debug("{}, {}".format(len(VDS), len(VDSAT)))
                 # Check that arrays VDS and VDSAT are the same length
                 if not (len(VDS) == len(VDSAT)):
-                    logging.critical("VDS/VDSAT length mismatch")
+                    Logger.critical("VDS/VDSAT length mismatch")
                     sys.exit()
                 found_v = [0, 0]
                 VSAT = [round(abs(VDS_i) - abs(VDSAT_i), 5) for VDS_i, VDSAT_i in zip(VDS, VDSAT)]
-                logging.info("VDS - VDSAT")
-                logging.info("{}".format(VSAT))
+                Logger.info("VDS - VDSAT")
+                Logger.info("{}".format(VSAT))
                 if any(VSAT_i < 0 for VSAT_i in VSAT):
-                    logging.info("VDS:   {}".format(VDS))
-                    logging.info("VDSAT: {}".format(VDSAT))
-                    logging.info("VSAT:  {}".format(VSAT))
-                    logging.info("Found MOSFET out of saturation")
+                    Logger.info("VDS:   {}".format(VDS))
+                    Logger.info("VDSAT: {}".format(VDSAT))
+                    Logger.info("VSAT:  {}".format(VSAT))
+                    Logger.info("Found MOSFET out of saturation")
                     sys.exit()
                 print ""
-    logging.info("All MOSFETS are in saturation!")
+    Logger.info("All MOSFETS are in saturation!")
 
 # This function finds then parses the information in the appropriate
 def findLine(name, found, line):
     if name in line:
         if found == 1:
-            logging.critical("Second {} found before other parameters".format(name.strip()))
+            Logger.critical("Second {} found before other parameters".format(name.strip()))
             sys.exit()
-        logging.debug("Found {}".format(name.strip()))
+        Logger.debug("Found {}".format(name.strip()))
         params = line.split()[1:]
         params = [float(param) for param in params]
-        logging.debug(params)
+        Logger.debug(params)
         return (params, 1)
     return ([], 0)
 
 if __name__ ==   "__main__":
-    #logging.basicConfig(level=LOG_LVL, format = "%({0: <6})s - %(message)s".format(logging.getLevelName(LOG_LVL)))
-    logging.basicConfig(level=LOG_LVL)
+    #Logger.basicConfig(level=LOG_LVL, format = "[%(levelname -8s]: %(name)-15s: %(funcname)-15s: %(lineno)-4d >> %(message)s")
+    #Logger.basicConfig(level=LOG_LVL)
+    logging.basicConfig(format = "[%(levelname) -8s]: %(name)-15s: %(funcName)-10s: %(lineno)-4d >> %(message)s")
+    Logger = logging.getLogger("PSPICE_Out_Parse")
+    Logger.setLevel(LOG_LVL)
     if len(sys.argv) != 2:
-        logging.critical("Use as \"python proc_out_file.py <filename>\"")
+        Logger.critical("Use as \"python proc_out_file.py <filename>\"")
         sys.exit()
     if not os.path.isfile(sys.argv[1]):
-        logging.critical("Invalid file name")
+        Logger.critical("Invalid file name")
         sys.exit()
-    main()
+    main(Logger)
